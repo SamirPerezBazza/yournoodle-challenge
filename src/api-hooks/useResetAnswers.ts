bb98fcce82a3368546c3f7fdeae119df7f0c7f1a
Samir Perez <saperez@mgmresorts.com>
2024-07-17 17:20:52 -0500

    feat: created hook to reset the answers of the table

--- src/api-hooks/useResetAnswers.ts
+++ src/api-hooks/useResetAnswers.ts
@@ -8,4 +8,33 @@ import { useAnswersStore } from '../state'
 // - You need to implement a new hook called useResetAnswers.
 // - Once implemented, you should be able to use this hook in the Table view.
 
-export const useResetAnswers = () => {}
+export const useResetAnswers = () => {
+    const answers = useAnswersStore(state => state)
+    const resetAnswersMutation = useMutation(() => resetAnswersFromApi())
+
+    const reset = () => {
+        resetAnswersMutation.mutate()
+        answers.setAnswers(
+            apiToDomainAnswersConverter({
+                age: '',
+                interests: answers.interests.map(option => {
+                    const key = Object.keys(option)[0]
+
+                    return {
+                        [Number(key)]: {
+                            ...option[Number(key)],
+                            isChecked: false,
+                            checked: false,
+                        },
+                    }
+                }),
+                email: '',
+                username: '',
+            }),
+        )
+    }
+
+    return {
+        reset,
+    }
+}
